f0f68058c251bb5fb59c10a2f430b79706756c1b
junghl11 <junghl11@naver.com>
2024-11-18 10:34:43 +0900

    add main

---
+++ TailorLink_Back/src/main/java/studio/thinkground/tailorlink/TailorLinkApplication.java
@@ -0,0 +1,13 @@
+package studio.thinkground.tailorlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailorLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TailorLinkApplication.class, args);
+    }
+
+}
---
+++ TailorLink_Back/src/test/java/studio/thinkground/tailorlink/TailorLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package studio.thinkground.tailorlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TailorLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
